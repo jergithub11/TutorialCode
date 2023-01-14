8b2bb50c3c1e6cdc914909cc444b6a93bf45c615
jergithub11 <121175507+jergithub11@users.noreply.github.com>
2023-01-14 11:25:24 +0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,164 +1,172 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(kalkulator());
+  runApp(const UAS());
 }
 
-class kalkulator extends StatefulWidget {
-  const kalkulator({super.key});
+class UAS extends StatelessWidget {
+  const UAS({super.key});
 
-  @override
-  State<kalkulator> createState() => _kalkulatorState();
-}
-
-class _kalkulatorState extends State<kalkulator> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('KALKULATOR'),
-          leading: Icon(Icons.dashboard),
-          backgroundColor: Colors.black,
-        ),
-        body: Center(
-          child: Container(
-            alignment: Alignment.topCenter,
-            padding: EdgeInsets.all(30),
-            child: Column(children: [
-              Image.asset(
-                'assets/images/logo.png',
-                width: 150,
+      title: 'Kalkulator Cok!',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: const MyHomePage(title: 'Kalkulator Spinner'),
+    );
+  }
+}
+
+// Oprator
+const List<String> list = <String>[
+  '- Pilih Operasi -',
+  'Kali',
+  'Bagi',
+  'Tambah',
+  'Kurang',
+  'Genap',
+  'Ganjil'
+];
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String dropdownValue = list.first;
+  var bil1 = 0, bil2 = 0, sum = 0;
+  final TextEditingController t1 = TextEditingController();
+  final TextEditingController t2 = TextEditingController();
+
+  void doAddition() {
+    setState(() {
+      bil1 = int.parse(t1.text);
+      bil2 = int.parse(t2.text);
+
+      sum = bil1 + bil2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      bil1 = int.parse(t1.text);
+      bil2 = int.parse(t2.text);
+
+      sum = bil1 - bil2;
+    });
+  }
+
+  void doMul() {
+    setState(() {
+      bil1 = int.parse(t1.text);
+      bil2 = int.parse(t2.text);
+
+      sum = bil1 * bil2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      bil1 = int.parse(t1.text);
+      bil2 = int.parse(t2.text);
+
+      sum = bil1 ~/ bil2;
+    });
+  }
+
+  void ganjil() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.dashboard),
+        title: const Text("Kalkulator Spinner"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: Text(
+                "Kalkulator Spinner\n     (KALSPIN)",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
               ),
-              Text(
-                'Kalkulator UBG',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  hintText: "Masukkan nilai 1 / Nilai Awal"),
+              controller: t1,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  hintText: "Masukkan nilai 2 / Nilai Akhir"),
+              controller: t2,
+            ),
+          
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.architecture),
+              elevation: 16,
+              isExpanded: true,
+              
+              style: const TextStyle(color: Colors.black),
+              onChanged: (String? value) {
+                
+                // This is called when the user selects an item.
+                setState(() {
+                  dropdownValue = value!;
+                });
+              },
+              items: list.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (dropdownValue == 'Kali') {
+                    doMul();
+                  } else if (dropdownValue == "Tambah") {
+                    doAddition();
+                  } else if (dropdownValue == "Bagi") {
+                    doDiv();
+                  } else if (dropdownValue == "Kurang") {
+                    doSub();
+                  }
+                },
+                child: const Text("Hitung "),
               ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Nilai pertama',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(28),
-                            borderSide: BorderSide(),
-                          ),
-                          labelText: 'Masukan Nilai Pertama'),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Nilai Kedua',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(28),
-                            borderSide: BorderSide(),
-                          ),
-                          labelText: 'Masukan Nilai Kedua'),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          SizedBox(width: 50),
-                          ElevatedButton(
-                            child: Text('x'),
-                            onPressed: () {
-                              // Tambahkan aksi untuk tombol kali disini
-                            },
-                          ),
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            child: Text('/'),
-                            onPressed: () {
-                              // Tambahkan aksi untuk tombol tambah disini
-                            },
-                          ),
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            child: Text('+'),
-                            onPressed: () {
-                              // Tambahkan aksi untuk tombol kurang disini
-                            },
-                          ),
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            child: Text('-'),
-                            onPressed: () {
-                              // Tambahkan aksi untuk tombol kurang disini
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: [
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              'Hasil',
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
-                            ),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(28),
-                                  borderSide: BorderSide(),
-                                ),
-                                labelText: 'Hasil'),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            child: Column(
-                              children: [
-                                SizedBox(width: 50),
-                                ElevatedButton(
-                                  child: Text('='),
-                                  onPressed: () {
-                                    // Tambahkan aksi untuk tombol kali disini
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ]),
-          ),
+            ),
+            Text(
+              "hasil : $sum",
+              style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ],
         ),
       ),
     );
